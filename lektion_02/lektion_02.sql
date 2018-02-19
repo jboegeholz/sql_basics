59d940dad5ce818434d38c031e332b7a9bc7b930
jobo8505 <boegeholz.joern@gmail.com>
2018-02-19 17:10:44 +0100

    add lektion 02

---
+++ lektion_02/lektion_02.sql
@@ -0,0 +1,5 @@
+CREATE TABLE missions (id INTEGER, code_name TEXT, operative_id INTEGER, total_cost DECIMAL(15,2), PRIMARY KEY(id), FOREIGN KEY (operative_id) REFERENCES operatives(id));
+INSERT INTO missions VALUES (1, "Knight of the Phoenix", 1, 6246382.43);
+INSERT INTO missions VALUES (2, "Deadly Maneuvers", 1, 4568893.53);
+
+SELECT * FROM missions LEFT JOIN operatives ON missions.operative_id = operatives.op_id;
